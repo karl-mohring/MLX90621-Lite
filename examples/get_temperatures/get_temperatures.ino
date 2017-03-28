da549fbfd10794f824d437041faf678735b265b3
Leenix <leenix48@gmail.com>
2017-03-28 16:53:29 +1000

    Fixed major bug when getting frames - Output was being incorrectly sorted into rows and columns

--- examples/get_temperatures/get_temperatures.ino
+++ examples/get_temperatures/get_temperatures.ino
@@ -1,4 +1,5 @@
 #include "Arduino.h"
+#include "i2c_t3.h"
 #include "MLX90621.h"
 
 int refresh_rate = 16;
@@ -7,13 +8,24 @@ MLX90621 sensor;
 float temperatures[64];
 
 void setup(){
+    Serial.begin(115200);
     Serial.println("Starting MLX90621 thermopile sensor");
-    sensor.initialize(refresh_rate);
+    pinMode(D1, OUTPUT);
+    digitalWrite(D1, HIGH);
+
+    Wire.begin(D2, D3);
+    sensor.initialise(refresh_rate);
 }
 
 void loop(){
-    Serial.println("/n/nReading sensor...");
+    Serial.println("\n\nReading sensor...");
+
+    long start_time = millis();
     sensor.get_temperatures(temperatures);
+    long time_taken = millis() - start_time;
+
+    Serial.print("Time taken: ");
+    Serial.println(time_taken);
 
     for(int y=0;y<64;y+=16){ //go through all the rows
         Serial.print('[');
@@ -22,9 +34,14 @@ void loop(){
 
             if (x < 15) {
                 Serial.print(", ");
+            }
         }
+        
         Serial.println(']');
     }
-
+    
+    Serial.print("Ambient temperature: ");
+    Serial.print(sensor.get_ambient_temperature());
+    Serial.println("°C");
     delay(500);
 }
